3d03cdfc3d53551948f4d28ce2c833486c7196d2
gitisthebest31 <myungwan31@gmail.com>
2025-06-28 22:55:40 +0900

    'POST 구현 초안'

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-from views import user
+
 app.register_blueprint(user.bp)
 
 @app.route('/')
 def home():
    return render_template('index.html')
+
+
 
 if __name__ == '__main__':
    app.run('0.0.0.0', port=5000, debug=True)
--- views/user.py
+++ views/user.py
@@ -9,6 +9,10 @@ client = MongoClient(uri, 27017) #MongoDB는 27017 포트로 돌아갑니다.
 db = client.user
 
 
+ # MongoDB에 insert 하기
+    
+#     # 'users'라는 collection에 {'ID':'myungwan31','PW':jungleFighting}를 넣습니다.
+# db.users.insert_one({'ID':'myungwan31','PW':'jungleFighting'})
 
 
 
@@ -28,3 +32,19 @@ def read_users():
     # }    
     # 2. input-ID라는 키 값으로 ID 정보 보내주기
     
+
+@bp.route('',methods=['POST'])
+def login_users():
+    print("요청보냄")
+    # user_ID= request.args.get("user")
+    # print(user_ID)
+
+    data = request.get_json()
+    print("받은 데이터:", data)
+
+    user_id = data.get('id')
+    user_pw = data.get('pw')
+    print("ID: {user_id}, PW: {user_pw}")
+
+
+    return jsonify({'result': 'success', 'msg': 'POST 연결되었습니다!'})
