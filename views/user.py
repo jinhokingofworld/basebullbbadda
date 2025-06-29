7daf0b9ff3083ef8facb67cd4399c2233dc4eb66
gitisthebest31 <myungwan31@gmail.com>
2025-06-29 16:46:55 +0900

    '회원가입 백엔드'

--- views/user.py
+++ views/user.py
@@ -7,7 +7,7 @@ user_page = Blueprint('user', __name__, static_folder="static", template_folder=
 
 uri = "mongodb+srv://gksqkf0824:iIb12ywrv7wlB0BP@kwonsoyun.xkcilez.mongodb.net/?retryWrites=true&w=majority&appName=KWONSOYUN&tlsAllowInvalidCertificates=true"
 client = MongoClient(uri, 27017) #MongoDB는 27017 포트로 돌아갑니다.
-db = client.user
+db = client.Splint2_Database
 
 
  # MongoDB에 insert 하기
@@ -16,13 +16,37 @@ db = client.user
 # db.users.insert_one({'ID':'myungwan31','PW':'jungleFighting'})
 
 
-
-
-
-
-@user_page.route('/')
+# 회원가입 api 응답하는 부분
+@user_page.route('/api/signin', methods=['POST'])
 def read_users():
-    return render_template('login.html')
+    # DB에 ID,PW,nickname 데이터 저장
+    ID = request.form['id']
+    PW = request.form['pw']
+    Nickname =request.form['nickname']
+    
+    #DB에 중복여부 체크후 중복시 메세지 반환후 종료
+    IDcheck = db.user.find_one({'id': ID})
+    if ID == IDcheck:
+        return jsonify({'msg' : '중복된 아이디입니다.','id' : ID})
+    
+    
+
+
+    # DB에 도큐먼트 형태로 변환
+    doc = { 'id' : ID, 'pw': PW, 'nickname': Nickname, 'idol' :'', 'likes': [] }
+    # DB에 저장       
+    db.user.insert_one(doc)
+
+    # 가입 성공
+    return jsonify({'msg' :'회원가입 성공하셨습니다.', 'nickname' : Nickname })
+
+    
+    
+
+# 로그인 페이지 라우팅
+@user_page.route('/')
+def routing_users():
+    return render_template('signin.html')
     
 
 # @user_page.route('/signin', methods=['POST'])
