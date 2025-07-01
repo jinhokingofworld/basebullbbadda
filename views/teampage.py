0b98f9ec37e89300fc7543c0f996680dfe7b58bc
gitisthebest31 <myungwan31@gmail.com>
2025-07-01 17:17:04 +0900

    '댓글 등록 점검 프린트 삭제'

--- views/teampage.py
+++ views/teampage.py
@@ -217,19 +217,15 @@ def get_team_comments(team_id):
 # 댓글 등록 api 응답하는 부분
 @team_page.route('/<team_id>/comment', methods =['POST'])
 def post_comment(team_id):
-    print("✅ 댓글 등록 요청 도착함")
-    print(team_id)
-    # if 'id' not in session:
-    #     return jsonify({'result': 'fail', 'msg': '로그인이 필요합니다.', 'url': '/user/login'})
-    
+   
+ 
      # 입력받은 Comment 데이터 가져오기
     input_comment = request.form.get('comment','').strip()
 
     if(input_comment == ''):
         return jsonify({'result': 'fail', 'msg': '내용을 입력하세요.'})
 
-    
-    print(f"내용: {input_comment}, 팀: {team_id}")
+
     #세션된 ID,닉네임 가져오기
     target_user = session.get('id')
     target_nickname = session.get('nickname')
@@ -239,16 +235,7 @@ def post_comment(team_id):
         'team_id': team_id, 'id' : target_user, 'nickname' :target_nickname, 'comment' : input_comment,  'date': datetime.now().strftime('%Y-%m-%d %H:%M:%S') } #좋아요 추가시 user db와 이름혼동문제 
     
     # DB에 아이디, 닉네임과 댓글 내용 데이터 저장
-    comment_value = db.team_comment.insert_one(doc)
-    if comment_value != None :
-        print("✅ DB 저장 완료")
-    else:
-        print("DB문제임 수정하삼.")
     
     # 댓글 등록 성공 (응답 성공 반환)
     return jsonify( {'result': 'success','msg' : '댓글 등록 성공!', 'nickname' : target_nickname, 'url' : team_id})
     
-    # #id와 nickname을 세션 등록
-    # session['id'] = target_user['id']
-    # session['nickname'] = nickname
-    # return jsonify({'result': 'success', 'msg' : nickname + '님 환영합니다.', 'nickname' : nickname, 'url' : '/'})
